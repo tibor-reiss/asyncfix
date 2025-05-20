1eeadaac4d4a6e317024a3d9f882e41c3761dc5a
Tibor Reiss <tibor.reiss@gmail.com>
2025-05-20 14:03:41 +0200

    Fix type hints

--- asyncfix/journaler.py
+++ asyncfix/journaler.py
@@ -110,7 +110,7 @@ class Journaler:
             i_end = msg.index(b"\x01", i_start + 1)
             return int(msg[i_start + 4 : i_end])
         except Exception:
-            raise FIXMessageError(f"tag 34 is not found or invalid, in message: {msg}")
+            raise FIXMessageError(f"tag 34 is not found or invalid, in message: {msg.decode(encoding='utf-8')}")
 
     def set_seq_num(
         self,
@@ -196,7 +196,7 @@ class Journaler:
         session: FIXSession,
         direction: MessageDirection,
         seq_no: int,
-    ) -> bytes:
+    ) -> bytes | None:
         """Loads specific message from DB by seq no.
 
         Args:
